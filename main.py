5c4c56a66a9e6a387e6d5b04b6dc32fe36fc5d61
Daydreamer <terry160434@gmail.com>
2015-04-03 23:45:47 +0800

    add file I/O in UNIX-BASED SYSTEM

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 import sys       # working for arguments
 import slacker
 import datetime
+import codecs    # File I/O with UTF-8
 
 
 token = 'xoxp-2734280152-4137412349-4279772753-169911'
 slack = slacker.Slacker(token)
 date = datetime.datetime.now()
 channel = 'general'
-#filename = '{}_backup_{:%Y%m%d}.md'.format(channel,date)
+#filename = '{}_backup_{:%Y%m%d}'.format(channel,date)
+filename = 'backup.txt'
 
 def make_user_dict():
     l = slack.users.list()
@@ -31,26 +33,26 @@ print('id = {}'.format(_id))
 response = slack.channels.history(_id, count = 10)
 ts = 0
 dic = make_user_dict();
+output_file = codecs.open(filename, "w", "utf-8-sig")
 
 
-#while(response.body['has_more'] == True):
-
-last_user = '';
-for msg in response.body['messages']:
-    if msg['type'] == 'message':
-        if 'subtype' in msg:
-            if msg['subtype'] == "file_share":
-                print(msg['text'])
+while(response.body['has_more'] == True):
+    last_user = '';
+    for msg in response.body['messages']:
+        if msg['type'] == 'message':
+            if 'subtype' in msg:
+                if msg['subtype'] == "file_share":
+                    output_file.write(msg['text'] + '\n')
+                else:
+                    print('Subtype exists. Pass this line for later edit.')
             else:
-                print('Subtype exists. Pass this line for later edit.')
-        else:
-            if msg['user'] != last_user:
-                last_user = msg['user']
-                print(dic[msg['user']], ':')
-            print(msg['text'])
-            print('')
-            ts = msg['ts']
+                if msg['user'] != last_user:
+                    last_user = msg['user']
+                    output_file.write(dic[msg['user']] + ': \n')
+                output_file.write('\t' + msg['text'] + '\n')
+                ts = msg['ts']
+    print('Latest Timestamp:', ts)
+    break
 
-print('Latest Timestamp:', ts)
-#    break
+output_file.close()
 
