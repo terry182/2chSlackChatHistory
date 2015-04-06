d5fbb6c105f055ddef943a56f42d6754c669f591
Daydreamer <terry160434@gmail.com>
2015-04-06 23:11:11 +0800

    * Regular Expression add for handling @ operation * Different type of message handling added.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from msg import Msg
 import codecs    # File I/O with UTF-8
 
 #User-defined Arguments
-token = 'your-token'
+token = 'xoxp-2734280152-4137412349-4293823360-a7d3bf'
 slack = slacker.Slacker(token)
 channel = 'general'
 #filename = '{}_backup_{:%Y%m%d}'.format(channel,date)
@@ -40,10 +40,30 @@ _str1001 = '''
 
 def make_user_dict():
     l = slack.users.list()
-    dic = {}
+    dic = {'USLACKBOT': 'slackbot'}
     for user in l.body['members']:
         dic[user['id']] = user['name']
     return dic
+
+def out_1001(output_file):
+    output_file.write('1001 ：１００１：Over 1000 Thread \n')
+    output_file.write(_str1001)
+
+def MessageReading(message, MessageList, UserDic):
+    if 'subtype' not in message:
+        MessageList.append(Msg(message['user'], message['ts'], message['text']))
+    elif message['subtype'] == 'bot_message':
+        print('Bot message Exists.')
+        UserDic[message['bot_id']] = message['user_name']
+        MessageList.append(Msg(message['bot_id'], message['ts'], message['text'], message['subtype']))
+    elif message['subtype'] == 'me_message':
+        MessageList.append(Msg(message['user'], message['ts'], message['text'], message['subtype']))
+    elif message['subtype'] == 'message_changed':
+        MessageList.append(Msg(message['user'], message['ts'], message['text'], message['subtype'], edit_user = message['edited']['user'], edit_ts = message['edited']['ts']))
+    else:
+        pass
+
+
 
 
 # Getting Channel ID
@@ -53,12 +73,15 @@ for chan in re.body['channels']:
     if (chan['name'] == channel):
         _id = chan['id']
 
-print('id = {}'.format(_id))
+print('Channel id = {}'.format(_id))
 
 # Getting History
 response = slack.channels.history(_id, count = 1000)
 if response.body['messages']:
     print('Message Exists. {0} message(s) found.'.format(len(response.body['messages'])))
+else:
+    print('No message exists.')
+
 ts = 0
 dic = make_user_dict()
 l = []
@@ -81,31 +104,26 @@ while(response.body['has_more'] == True):
             if msg.user not in dic:
                 dic[msg.user] = msg.user
             cnt = cnt + 1
-            output_file.write(str(cnt) + '名前: {0} : {1} ID:{2} \n'.format(dic[msg.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(msg.ts))), msg.user))
-            output_file.write('\t' + msg.getTextAs2CH() + '\n')
-            output_file.write('1001 ：１００１：Over 1000 Thread \n')
-            output_file.write(_str1001)
+            msg.writeToFile(output_file, dic, cnt)
+        out_1001(output_file)
         l = []
         response = slack.channels.history(_id, latest = ts, count = 1000)
         #print('Latest Timestamp:', ts)
 else:
     for msg in response.body['messages']:
-        if 'subtype' in msg:
-            pass
-            #l.append(Msg(msg['user'], msg['ts'], msg['text'], msg['subtype']))
-        else:
-            l.append(Msg(msg['user'], msg['ts'], msg['text']))
+        MessageReading(msg, l, dic)
     if l:
         l.reverse()
         for msg in l:
             if msg.user not in dic:
                 dic[msg.user] = msg.user
-            output_file.write('名前: {0} : {1} ID:{2}'.format(dic[msg.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(msg.ts))), msg.user) + '\n')
-            output_file.write('\t' + msg.getTextAs2CH() + '\n')
-            #msg.print_(dic)
+            cnt = cnt + 1
+            msg.writeToFile(output_file, dic, cnt)
+            #msg.print_(dic, cnt)
+
         if len(l) == 1000:
-            output_file.write('1001 ：１００１：Over 1000 Thread \n')
-            output_file.write(_str1001)
+            out_1001(output_file)
+
         l = []
 
 output_file.close()
--- msg.py
+++ msg.py
@@ -1,12 +1,45 @@
 import time
+import re       #Regular Expression
+
 class Msg:
-    def __init__(self, user, ts, text, subtype = 0):
+    def __init__(self, user, ts, text, subtype = 0, **kwargs):
         self.user = user
         self.ts = ts
         self.text = text
         self.subtype = subtype
-    def getTextAs2CH(self):
-        return self.text.replace('\n', '\n\t')
-    def print_(self, dic):
-        print('名前: {0} : {1} ID:{2}'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user))
-        print('\t' + self.getTextAs2CH())
+        if subtype == 'message_changed':
+            self.edit_user = kwargs['edit_user']
+            self.edit_ts = kwargs['edit_ts']
+
+    def getTextAs2CH(self, dic):
+        self.text = re.sub('<@([A-Z0-9]+)>', r'@\1', self.text)
+        return  self.text.replace('\n', '\n\t')
+
+    def writeToFile(self, output_file, dic, cnt):
+        if self.subtype == 'bot_message':
+            output_file.write(str(cnt) + ' BOT名前: {0} : {1} ID:{2}\n'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user))
+            output_file.write('\t' + self.getTextAs2CH(dic) + '\n')
+        elif self.subtype == 'me_message':
+            output_file.write(str(cnt) + ' 名前: {0} : {1} ID:{2}\n'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user) + 'Type: /me message')
+            output_file.write('\t' + self.getTextAs2CH(dic) + '\n')
+        elif self.subtype == 'message_changed':
+            output_file.write(str(cnt) + ' 名前: {0} : {1} ID:{2}\n'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user) + 'Type: /me message')
+            output_file.write('\t' + self.getTextAs2CH(dic) + '\n')
+            output_file.write('Edited by {0} at {1}\n'.format(dic[self.edit_user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.edit_ts)))))
+        else:
+            output_file.write(str(cnt) + ' 名前: {0} : {1} ID:{2}\n'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user))
+            output_file.write('\t' + self.getTextAs2CH(dic) + '\n')
+
+    def print_(self, dic, cnt):
+        if self.subtype == 'bot_message':
+            print(str(cnt) + ' BOT名前: {0} : {1} ID:{2}'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user))
+            print('\t' + self.getTextAs2CH(dic))
+        elif self.subtype == 'me_message':
+            print(str(cnt) + ' 名前: {0} : {1} ID:{2}'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user) + 'Type: /me message')
+            print('\t' + self.getTextAs2CH(dic))
+        elif self.subtype == 'message_changed':
+            print(str(cnt) + ' 名前: {0} : {1} ID:{2}'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user) + 'Type: /me message')
+            print('\t' + self.getTextAs2CH(dic))
+            print('Edited by {0} at {1}'.format(dic[self.edit_user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.edit_ts)))))
+        else:
+            print(str(cnt) + ' 名前: {0} : {1} ID:{2}'.format(dic[self.user], time.strftime("%Y/%m/%d %a %H:%M:%S", time.localtime(float(self.ts))), self.user))
